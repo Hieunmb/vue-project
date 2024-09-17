1ed5d86b44b5c93ac6f792e8d88d5c220e98f2bc
Hieunmb <zhuongdae@gmail.com>
2024-09-17 21:40:05 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+<Preloader></Preloader>
+</template>
+
+<script setup>
+import Preloader from './components/preloader.vue';
+
+
+</script>
+
+
---
+++ src/components/preloader.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="preloader">
+        <div id="status">
+            <div class="spinner-border text-primary avatar-sm" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+</script>
